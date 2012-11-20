53ff3cb294fa635ac00e079d993cd6b4d2e90f28
Andrey Vasenin <vaseninlol@gmail.com>
2012-11-20 23:34:41 +0400

    Add path initializer for groups

--- example/db/migrate/items/native/patches/patch-2010-06-21.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX IDX_Origin_ItemType ON Items (ItemOriginURI, ItemType, ItemState, ItemCreationTimestamp);
-CREATE INDEX IDX_Origin_ItemTypeTS ON Items (ItemOriginURI, ItemType, ItemCreationTimestamp, ItemState);
-
-CREATE INDEX IDX_Parent_ItemType ON Items (ItemParentURI, ItemType, ItemState, ItemCreationTimestamp);
-CREATE INDEX IDX_Parent_ItemTypeTS ON Items (ItemParentURI, ItemType, ItemCreationTimestamp, ItemState);
