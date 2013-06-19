2281f8d49fbb9bb822fdccecd04c16525ec84ff8
Alex Peshkov <alp@aboutecho.com>
2013-06-19 16:26:53 +0400

    replaced 'local' and 'production' functions with 'env?'

--- lib/dbmigrator/environment.rb
+++ lib/dbmigrator/environment.rb
@@ -2,14 +2,10 @@ require 'active_record/migration'
 
 module DbMigrator
   module Environment
-    def production?
-      "production" == ENV["DBM_ENV"]
+    def env?
+      ENV["DBM_ENV"]
     end
-
-    def local?
-      !production?
-    end
-
+   
     module_function :production?, :local?
   end
 end
